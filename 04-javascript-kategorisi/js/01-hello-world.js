81fc1e60ebaf0b3ea4966ec47ac9febb86cebd50
mikailcskn <mikailcsknn@gmail.com>
2023-10-03 21:58:18 +0300

    asd

---
+++ 04-javascript-kategorisi/js/01-hello-world.js
@@ -0,0 +1,10 @@
+// ********** Merhaba Dunya Ornekleri ********** 
+
+// Dokumana(document) Yazdirma Islemi ile Merhaba Dunya yazımı:
+// document.write('Merhaba Dunya');
+
+// Uyari Mesaji (Alert) ile Merhaba Dunya yazımı:
+// alert("Merhaba Dunya")
+
+// console.log ile Merhaba Dunya:
+console.log("Merhaba Dunya")
---
+++ 04-javascript-kategorisi/js/02-degisken-tanimlama.js
@@ -0,0 +1,22 @@
+
+//var ile değişken tanımlamak
+var ServerName = "mikoo.com"
+console.log("Miko")
+
+//let ile değişken tanımlamak ve değiştirmek
+let serverName;
+console.log(serverName)
+
+serverName = "https://instagram.com"
+console.log(serverName)
+
+
+let password ="1111";
+console.log(password)
+
+let fullName = "Mikail CUŞKUN";
+console.log(fullName)
+
+//const ile değisken tanımlamak
+const SERVER_PASSWORD = "14sdasdasdd"
+console.log(SERVER_PASSWORD)
